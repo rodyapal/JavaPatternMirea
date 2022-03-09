cee29fdce69f0c625b7c9a6a29d5e6161f726ef4
Rodion Sviridov <sviridov.rod@yandex.ru>
2022-03-09 13:05:46 +0300

    Task 11 done

---
+++ task11_spring_boot/src/main/java/com/rodyapal/task11_spring_boot/Task11SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rodyapal.task11_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task11SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task11SpringBootApplication.class, args);
+	}
+
+}
